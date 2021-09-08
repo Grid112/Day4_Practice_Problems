ff303c11e8c9e06b3ad5632598c0e7753956f949
Grid112 <sumitware1999@gmail.com>
2021-09-08 20:13:30 +0530

    Player at Position 0

---
+++ src/com/company/Snake_Ladder.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int playerposition = 0;
+            System.out.println("The position of player is : " + playerposition);
+        }
+
+    }
+
